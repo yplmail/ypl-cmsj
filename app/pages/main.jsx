47e22d7a1f4449d5e9234797727ed08bf0f6623d
黎艳平 <liyanping@tophlc.com>
2017-02-15 13:43:36 +0800

    update

--- app/pages/main.jsx
+++ app/pages/main.jsx
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory,IndexRoute,Redirect} from 'react-router';
 import advNav    from './nav.jsx';
 import advList   from './advertisement/list.jsx';
-import advDetail from './advertisement/detail.jsx';
-import advMine   from './mine/Mine.jsx';
-import advHot    from './hot/hot.jsx';
 import advLogin from './login/login.jsx';
+import advHot    from './hot/hot.jsx';
+import advMine   from './mine/mine.jsx';
+import advDetail from './advertisement/detail.jsx';
 import advRegister from './register/register.jsx';
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={advNav}>
-      <IndexRoute component={advList} />
-      <Route path="login"  component={advLogin} />
-      <Route path="hot"    component={advHot} />
-      <Route path="mine"   component={advMine} />
-      <Route path="detail" component={advDetail} />
-      <Route path="register" component={advRegister} />
-    </Route>,
-    document.body.appendChild(document.createElement('div'))
+	<Router history={hashHistory}>
+		<Route path="/" component={advNav}>
+			<IndexRoute component={advList} />
+			<Route path="login"  component={advLogin} />
+			<Route path="hot"    component={advHot} />
+			<Route path="mine"   component={advMine} />
+			<Route path="detail" component={advDetail} />
+			<Route path="register" component={advRegister} />
+		</Route>
+	</Router>,
+	document.querySelector('.container')
 );
--- app/pages/nav.jsx
+++ app/pages/nav.jsx
@@ -9,12 +9,15 @@ class Nav extends React.Component{
 
 	render(){
        return(
-           <div className="nav-wrapper">
-              <ul data-flex="dir:left box:mean">
-                 <li><IndexLink to="/">首页</IndexLink></li>
-                 <li><Link to="/hot">热门</Link></li>
-                 <li><Link to="/mine">我的</Link></li>
-              </ul>
+       	   <div>
+       	       {this.props.children}
+	           <div className="nav-wrapper">
+	              <ul data-flex="dir:left box:mean">
+	                 <li><IndexLink to="/" activeClassName="active">首页</IndexLink></li>
+	                 <li><Link to="/hot"   activeClassName="active">热门</Link></li>
+	                 <li><Link to="/mine"  activeClassName="active">我的</Link></li>
+	              </ul>
+	           </div>
            </div>
        )
 	}	
