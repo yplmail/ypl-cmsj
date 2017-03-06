2ca0dd102bd0bde44535660c64c2d0b632fae72b
黎艳平 <liyanping@tophlc.com>
2017-03-06 18:48:24 +0800

    update

--- app/pages/mine/mine.jsx
+++ app/pages/mine/mine.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router';
+import common from '../../common/common';
 import './mine.css';
+
 class Mine extends React.Component{
    constructor(props){
-      super(props)
+      super(props);
+   }
+
+   componentDidMount(){
+
    }
 
    render(){
@@ -11,6 +17,7 @@ class Mine extends React.Component{
          <div className="mine-wrapper">
              <Header />
              <List />
+             <Mask/>
          </div>
       )
    }
@@ -24,9 +31,14 @@ class Header extends React.Component{
    render(){
       return (
           <div className="mine-header-wrapper">
-              <div><img src="../../images/user_header_icon.png" /></div>
-              <div><span>不仅仅是看客</span></div>
-              <div>获赠总金额&nbsp;￥<span>637.35</span></div>
+              <div className="personal-header">
+                  <p className="headerImg"></p>
+                  <p className="headerTip">不仅仅是看客</p>
+              </div>
+              <div className="personal-money">
+                  <span>获赠总额&nbsp;￥</span>
+                  <span>637.35</span>
+              </div>
           </div>
       )
    }
@@ -46,6 +58,33 @@ class List extends React.Component{
                   <li><Link>联系我们</Link></li>
                   <li><Link to="/setting">设置</Link></li>
               </ul>
+          </div>
+      )
+   }
+}
+
+class Mask extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         display : 'block'
+      }
+   }
+
+   componentDidMount(){
+      if(common.getcookies()){
+          this.setState({
+              display:'block'
+          })
+             //<Link to="/register">登陆</Link>
+      }
+   }
+
+   render(){
+      return (
+          <div className="mine-mask-wrapper" style={{display:this.state.display}}>
+              <div className="cercle"></div>
+              <Link className="cercle-login" to="/login">登录</Link>
           </div>
       )
    }
